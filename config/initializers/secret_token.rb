3c69a601ff03040ac31c53bc55bba2390008f4c1
htscs <web@htscs.com>
2012-01-09 15:03:15 +0100

    New year

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Grid960::Application.config.secret_token = '06b96530a4a3d98405948730ef4832e3224abbd44d3d7dc64dc0d28f2a87b7e5b8bcf81d8cbd05b6195323300eeecd1b3b276415c1ec045172e8bc9b994e9f60'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Grid960::Application.config.session_store :cookie_store, key: '_grid960_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Grid960::Application.config.session_store :active_record_store
